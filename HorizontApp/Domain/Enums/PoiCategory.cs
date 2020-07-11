dcf4411e8edfdfde1f88fc944dad93ba92c36b2b
franktom <tomas.frankl@tieto.com>
2020-07-11 11:44:14 +0200

    Added test points

--- HorizontApp/Domain/Enums/PoiCategory.cs
+++ HorizontApp/Domain/Enums/PoiCategory.cs
@@ -2,14 +2,16 @@
 {
     public enum PoiCategory
     {
-        Peaks,
-        Mountains,
-        Lakes,
-        Castles,
-        Palaces,
-        Ruins,
-        ViewTowers,
-        Transmitters,
-        Churches,
+        Peaks = 0,
+        Mountains = 1,
+        Lakes = 2,
+        Castles = 3,
+        Palaces = 4,
+        Ruins = 5,
+        ViewTowers = 6,
+        Transmitters = 7,
+        Churches = 8,
+
+        Test = 100
     }
 }
