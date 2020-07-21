90ebce5b0100f0a045cf35c46f02a909649bad18
Lukas Frankl <lukinfx@gmail.com>
2020-07-21 16:38:48 +0200

    CompassView folder

---
+++ HorizontApp/Views/Compass/CompassDrawer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using HorizontApp.Domain.ViewModel;
+
+namespace HorizontApp.Views.Compass
+{
+    public class CompassViewDrawer
+    {
+        protected Android.Graphics.Paint paint;
+        protected Android.Graphics.Paint paintRect;
+        protected Android.Graphics.Paint textpaint;
+
+        public float ViewAngleHorizontal { protected get; set; }
+        public float ViewAngleVertical { protected get; set; }
+
+        public virtual void Initialize()
+        {
+            paint = new Android.Graphics.Paint();
+            paint.SetARGB(255, 200, 255, 0);
+            paint.SetStyle(Paint.Style.FillAndStroke);
+            paint.StrokeWidth = 4;
+
+            paintRect = new Android.Graphics.Paint();
+            paintRect.SetARGB(150, 0, 0, 0);
+            paintRect.SetStyle(Paint.Style.FillAndStroke);
+            paintRect.StrokeWidth = 4;
+
+            textpaint = new Android.Graphics.Paint();
+            textpaint.SetARGB(255, 200, 255, 0);
+            textpaint.TextSize = 36;
+            Typeface normal = Typeface.Create("Arial", TypefaceStyle.Normal);
+            textpaint.SetTypeface(normal);
+        }
+
+        public virtual void OnDrawBackground(Android.Graphics.Canvas canvas) { }
+        public virtual void OnDrawItem(Android.Graphics.Canvas canvas, PoiViewItem item, float heading) { }
+    }
+}
---
+++ HorizontApp/Views/Compass/CompassView.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using HorizontApp.Domain.ViewModel;
+using HorizontApp.Providers;
+using HorizontApp.Utilities;
+using HorizontApp.Views.Compass;
+
+namespace HorizontApp.Views
+{
+
+    
+
+    public class CompassView : View
+    {
+        public static PoiViewItemList list;
+        public double Heading { get; set; }
+
+        private float viewAngleHorizontal;
+        private float viewAngleVertical;
+        public float ViewAngleHorizontal { 
+            get
+            {
+                return viewAngleHorizontal;
+            }
+            set 
+            { 
+                compassViewDrawer.ViewAngleHorizontal = value;
+                viewAngleHorizontal = value;
+            } 
+        }
+        public float ViewAngleVertical
+        {
+            get
+            {
+                return viewAngleHorizontal;
+            }
+            set
+            {
+                compassViewDrawer.ViewAngleVertical = value;
+                viewAngleVertical = value;
+            }
+        }
+        private CompassViewDrawer compassViewDrawer;
+
+        public CompassView(Context context, IAttributeSet attrs) :
+            base(context, attrs)
+        {
+            Initialize();
+        }
+
+        public void SetPoiViewItemList(PoiViewItemList list2)
+        {
+            list = list2;
+        }
+
+        public CompassView(Context context, IAttributeSet attrs, int defStyle) :
+            base(context, attrs, defStyle)
+        {
+            Initialize();
+        }
+
+        public void OnSettingsChanged(object sender, SettingsChangedEventArgs e)
+        {
+            InitializeViewDrawer();
+        }
+
+        private void Initialize()
+        {
+            CompassViewSettings.Instance().SettingsChanged += OnSettingsChanged;
+
+            InitializeViewDrawer();
+        }
+
+        private void InitializeViewDrawer()
+        {
+            switch (CompassViewSettings.Instance().AppStyle)
+                {
+                    case AppStyles.NewStyle:
+                        compassViewDrawer = new CompassViewDrawerNewStyle();
+                        break;
+                    case AppStyles.OldStyle:
+                        compassViewDrawer = new CompassViewDrawerOldStyle();
+                        break;
+                }
+
+            compassViewDrawer.ViewAngleHorizontal = ViewAngleHorizontal;
+            compassViewDrawer.ViewAngleVertical = ViewAngleVertical;
+            compassViewDrawer.Initialize();
+        }
+        protected override void OnDraw(Android.Graphics.Canvas canvas)
+        {
+            compassViewDrawer.OnDrawBackground(canvas);
+            
+            canvas.Rotate(90, 0, 0);
+
+            if (list != null)
+            {
+                foreach (var item in list)
+                {
+                    compassViewDrawer.OnDrawItem(canvas, item, (float)Heading);
+                }
+            }
+        }
+    }
+}
