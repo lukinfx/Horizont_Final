54e1946af483a61393ddcf960371fe4a7902ab32
Lukas Frankl <lukinfx@gmail.com>
2020-10-30 09:25:45 +0100

    MaxDist and MinAlt settings in PhotoShowActivity fixed

--- HorizontApp/Activities/PhotoShowActivity.cs
+++ HorizontApp/Activities/PhotoShowActivity.cs
@@ -101,6 +101,8 @@ namespace HorizontApp.Activities
             _context.Settings.Categories = JsonConvert.DeserializeObject<List<PoiCategory>>(photodata.JsonCategories);
             _context.Settings.SetCameraParameters((float)photodata.ViewAngleHorizontal, (float)photodata.ViewAngleVertical,
                 AppContextLiveData.Instance.Settings.CameraPictureSize.Width, AppContextLiveData.Instance.Settings.CameraPictureSize.Height);
+            _context.Settings.MaxDistance = Convert.ToInt32(photodata.MaxDistance);
+            _context.Settings.MinAltitute = Convert.ToInt32(photodata.MinAltitude);
 
             _filterText = FindViewById<TextView>(Resource.Id.textView1);
 
