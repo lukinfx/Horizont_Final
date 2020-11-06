cec2ba1f1d06a20a5e871abc382916f3ee28ad23
franktom <tomas.frankl@tieto.com>
2020-11-06 09:00:46 +0100

    New logo

--- HorizontApp/Activities/AboutActivity.cs
+++ HorizontApp/Activities/AboutActivity.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -21,7 +22,6 @@ namespace HorizontApp.Activities
 
             SetContentView(Resource.Layout.AboutActivity);
 
-
             var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetActionBar(toolbar);
 
@@ -29,7 +29,15 @@ namespace HorizontApp.Activities
             ActionBar.SetDisplayHomeAsUpEnabled(true);
             ActionBar.SetDisplayShowTitleEnabled(false);
             // Create your application here
+
+            var versionTextView = FindViewById<TextView>(Resource.Id.aboutVersion);
+            var packageInfo = Application.Context.ApplicationContext.PackageManager.GetPackageInfo(Application.Context.ApplicationContext.PackageName, 0);
+            var versionName = packageInfo.VersionName;
+            var versionCode = packageInfo.VersionCode;
+            var firstInstall = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(packageInfo.FirstInstallTime);
+            versionTextView.Text = $"Version: {versionName}.{versionCode}, installed on {firstInstall.ToShortDateString()}";
         }
+
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
             switch (item.ItemId)
