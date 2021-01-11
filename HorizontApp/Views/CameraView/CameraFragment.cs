d7e491910b9f28f911292b314e97fe5a08ef35a5
franktom <tomas.frankl@tieto.com>
2021-01-11 15:23:47 +0100

    ShowToastRunnable moved to separated class

--- HorizontApp/Views/CameraView/CameraFragment.cs
+++ HorizontApp/Views/CameraView/CameraFragment.cs
@@ -123,23 +123,6 @@ namespace HorizontApp.Views.Camera
             }
         }
 
-        private class ShowToastRunnable : Java.Lang.Object, IRunnable
-        {
-            private string text;
-            private Context context;
-
-            public ShowToastRunnable(Context context, string text)
-            {
-                this.context = context;
-                this.text = text;
-            }
-
-            public void Run()
-            {
-                Toast.MakeText(context, text, ToastLength.Short).Show();
-            }
-        }
-
         private static Size ChooseOptimalSize(Size[] choices, int textureViewWidth,
             int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio)
         {
