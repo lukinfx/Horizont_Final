858ed45a62a3cd619d047e0698dde547e590464b
franktom <tomas.frankl@tieto.com>
2021-01-23 18:46:04 +0100

    Added ImageCopySaver class

--- HorizontApp/Activities/PhotoShowActivity.cs
+++ HorizontApp/Activities/PhotoShowActivity.cs
@@ -555,67 +555,9 @@ namespace HorizontApp.Activities
 
         private void SaveCopy()
         {
-            var cropRect = photoView.CroppingRectangle;
-
-            var croppedBitmap = Bitmap.CreateBitmap(dstBmp, cropRect.Left, cropRect.Top, cropRect.Width(), cropRect.Height());
-
-            var viewAngleVertical = Context.ViewAngleVertical * croppedBitmap.Height / (double)dstBmp.Height;
-            var viewAngleHorizontal = Context.ViewAngleHorizontal * croppedBitmap.Width / (double)dstBmp.Width;
-
-            //center new, minu center old
-            var hdgCorrectionInPixels = ((cropRect.Left + cropRect.Right) / 2.0) - (dstBmp.Width / 2.0);
-            //and now to degrees
-            var hdgCorrectionInDegrees = hdgCorrectionInPixels / (float) dstBmp.Width * Context.ViewAngleHorizontal;
-
-            //Linear function - calculation of new corrector values
-            double leftCorrectionInDegrees = _compassView.LeftTiltCorrector + (_compassView.RightTiltCorrector - _compassView.LeftTiltCorrector) * (cropRect.Left / (double)dstBmp.Width);
-            double rightCorrectionInDegrees = _compassView.LeftTiltCorrector + (_compassView.RightTiltCorrector - _compassView.LeftTiltCorrector) * (cropRect.Right / (double)dstBmp.Width);
-
-            //New center of image can be somewhere else, so we need to reflect this in total view angle correction
-            double totalCorrectionInPixels = (((cropRect.Top + cropRect.Bottom) / 2.0) - (dstBmp.Height / 2.0));
-            double totalCorrectionInDegrees = (totalCorrectionInPixels / dstBmp.Height) * Context.ViewAngleVertical;
-
-            var now = DateTime.Now;
-
-            PhotoData newPhotodata = new PhotoData
-            {
-                Tag = "Copy of " + photodata.Tag,
-                Datetime = now,
-                PhotoFileName = ImageSaverUtils.GetPhotoFileName(now),
-                Longitude = photodata.Longitude,
-                Latitude = photodata.Latitude,
-                Altitude = photodata.Altitude,
-                Heading = Context.Heading + Context.HeadingCorrector + hdgCorrectionInDegrees,
-                JsonCategories = JsonConvert.SerializeObject(Context.Settings.Categories),
-                ViewAngleVertical = viewAngleVertical,
-                ViewAngleHorizontal = viewAngleHorizontal,
-                LeftTiltCorrector = leftCorrectionInDegrees + totalCorrectionInDegrees,
-                RightTiltCorrector = rightCorrectionInDegrees + totalCorrectionInDegrees,
-                PictureWidth = croppedBitmap.Width,
-                PictureHeight = croppedBitmap.Height,
-                MinAltitude = MinHeight,
-                MaxDistance = MaxDistance,
-                FavouriteFilter = Context.ShowFavoritesOnly,
-                ShowElevationProfile = Context.Settings.ShowElevationProfile
-            };
-
-            var thumbnainBitmap = Bitmap.CreateScaledBitmap(croppedBitmap, 150, 100, false);
-            using (MemoryStream ms = new MemoryStream())
-            {
-                thumbnainBitmap.Compress(Bitmap.CompressFormat.Jpeg, 70, ms);
-                newPhotodata.Thumbnail = ms.ToArray();
-            }
-            
-            if (_context.ElevationProfileData != null)
-            {
-                photodata.JsonElevationProfileData = Context.ElevationProfileData.Serialize();
-            }
-
-            var filePath = System.IO.Path.Combine(ImageSaverUtils.GetPhotosFileFolder(), newPhotodata.PhotoFileName);
-            var stream = new FileStream(filePath, FileMode.Create);
-            croppedBitmap.Compress(Bitmap.CompressFormat.Jpeg, 100, stream);
-
-            AppContextLiveData.Instance.PhotosModel.InsertItem(newPhotodata);
+            var newPhotoData = ImageCopySaver.Save(dstBmp, photoView.CroppingRectangle, photodata,
+                _compassView.LeftTiltCorrector, _compassView.RightTiltCorrector,MinHeight, MaxDistance, Context);
+            AppContextLiveData.Instance.PhotosModel.InsertItem(newPhotoData);
         }
     }
 }
