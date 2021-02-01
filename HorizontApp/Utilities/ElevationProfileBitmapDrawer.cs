6fe23a2ccb007310642735720a6b3882169792a5
franktom <tomas.frankl@tieto.com>
2021-02-01 19:59:42 +0100

    Better generation of ElevationProfileLines

--- HorizontApp/Utilities/ElevationProfileBitmapDrawer.cs
+++ HorizontApp/Utilities/ElevationProfileBitmapDrawer.cs
@@ -59,7 +59,22 @@ namespace HorizontApp.Utilities
                 {
                     foreach (var point in thisAngle.GetPoints())
                     {
-                        foreach (var otherPoint in prevAngle.GetPoints())
+                        var otherPoint = prevAngle.GetPoints()
+                            .Where(x => Math.Abs(point.Distance.Value - x.Distance.Value) <= point.Distance.Value / 12)
+                            .OrderBy(x => Math.Abs(point.Distance.Value - x.Distance.Value))
+                            .FirstOrDefault();
+
+                        if (otherPoint != null)
+                        {
+                            var y1 = point.VerticalViewAngle.Value;
+                            var x1 = (float) point.Bearing.Value;
+
+                            var y2 = otherPoint.VerticalViewAngle.Value;
+                            var x2 = (float) otherPoint.Bearing.Value;
+                            listOfLines.Add(new ProfileLine {Bearing1 = x1, Bearing2 = x2, VerticalViewAngle1 = (float) y1, VerticalViewAngle2 = (float) y2, distance = point.Distance.Value});
+                        }
+
+                        /*foreach (var otherPoint in prevAngle.GetPoints())
                         {
                             if (point.Bearing.HasValue && otherPoint.Bearing.HasValue && point.Distance.HasValue && otherPoint.Distance.HasValue && point.VerticalViewAngle.HasValue && otherPoint.VerticalViewAngle.HasValue)
                             {
@@ -73,7 +88,7 @@ namespace HorizontApp.Utilities
                                     listOfLines.Add(new ProfileLine { Bearing1 = x1, Bearing2 = x2, VerticalViewAngle1 = (float)y1, VerticalViewAngle2 = (float)y2, distance = point.Distance.Value });
                                 }
                             }
-                        }
+                        }*/
                     }
                 }
             }
